68c4ac7fc6fc2a3421fb7827beece956c8ea0c6c
Venkatesh Muthusamy <machovenki@gmail.com>
2016-02-13 21:51:17 -0600

    initial setup

---
+++ src/main/java/com/venki/test/ElysiumApplication.java
@@ -0,0 +1,12 @@
+package com.venki.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElysiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElysiumApplication.class, args);
+	}
+}
